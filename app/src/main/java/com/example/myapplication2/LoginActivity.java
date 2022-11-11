ddfe86dd107af10a64ff343b8d85eefdf89f6dd7
HongKheeChee <83328942+HongKheeChee@users.noreply.github.com>
2022-11-11 17:15:46 +0800

    complete register

--- app/src/main/java/com/example/myapplication2/LoginActivity.java
+++ app/src/main/java/com/example/myapplication2/LoginActivity.java
@@ -2,13 +2,26 @@ package com.example.myapplication2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.myapplication2.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private ActivityLoginBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.noAccTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
     }
 }
